8dc524c86258a8aed9ee384fe5ea85d34eafd933
Garrett Webb <garrettwebb97@gmail.com>
2021-04-09 11:17:16 -0700

    commenting code and updating readme

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Private repository for CSE138 - Distributed Sustems - Assignment 1
 * I used the built-in http server module in python, and used the docs here to get started. `https://docs.python.org/3/library/http.server.html`
 * I glanced at this website, which details the code structure of a simple httpserver implementation in python, but I did not copy any of the code aside from that which was used in the official python documentation. `https://stackabuse.com/serving-files-with-pythons-simplehttpserver-module/`
 * one student `kai hsieh` helped me with configuring docker on my computer, however no code was shared.
+
+## Notes
+* I am sorry about the fact that I added all the completed files in one go to this repository, I should have made the repo first and committed as I went, but I did not fully read the submission workflow part of the assignment until I had finished the code. I will remedy this in future assignments.
--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+#Garrett Webb
+#gswebb
+#CSE138 Kuper Spring 2021
+
+#acknowledgements
 #https://stackabuse.com/serving-files-with-pythons-simplehttpserver-module/
 #https://docs.python.org/3/library/http.server.html
 
@@ -7,21 +12,21 @@ import socketserver
 import json
 
 class requestHandler(http.server.SimpleHTTPRequestHandler):
-    
-    def send_response_code(self, response_code):
-        self.send_response(response_code)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-    
     def do_GET(self):
-        if str(self.path) == '/ping': #if there is something after the ping, this will not trigger
+        if str(self.path) == '/ping': 
+            #if there is something after the ping, this will not trigger
+
+            #send the 200 code
             self.send_response(200)
             self.send_header("Content-type", "text/html")
-            self.end_headers()
+            self.end_headers() #gotta do this for some reason
+
+            #send the json thing
             response = bytes(json.dumps({'message' : "I'm alive!!"}), 'utf-8')
             self.wfile.write(response)
         
         elif str(self.path).startswith("/ping"):
+            #if the string is not exactly /ping but starts with it, not allowed
             self.send_response(405)
             self.send_header("Content-type", "text/html")
             self.end_headers()
@@ -30,6 +35,7 @@ class requestHandler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(response)
 
         elif str(self.path) == '/echo':
+            #not a ping, but an echo
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
@@ -37,6 +43,7 @@ class requestHandler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(response)
 
         else:
+            #default 500, just for cleaning up loose ends
             self.send_response(500)
             self.send_header("Content-type", "text/html")
             self.end_headers()
@@ -45,6 +52,7 @@ class requestHandler(http.server.SimpleHTTPRequestHandler):
     
     def do_POST(self):
         if str(self.path) == '/ping':
+            #if there is something after the ping, this will not trigger
             self.send_response(405)
             self.send_header("Content-type", "text/html")
             self.end_headers()
@@ -53,14 +61,17 @@ class requestHandler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(response)
         
         elif str(self.path).startswith("/ping"):
+            #if starts with ping, but has something after, send 200
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
+            #get the substring after the /ping/
             response_str = str(self.path).split("/ping/",1)[1]
             response = bytes(json.dumps({'message' : "I'm alive, " + response_str + "!!"}), 'utf-8')
             self.wfile.write(response)
         
         elif str(self.path) == '/echo':
+            # not a ping, but an echo
             self.send_response(400)
             self.send_header("Content-type", "text/html")
             self.end_headers()
@@ -68,15 +79,17 @@ class requestHandler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(response)
         
         elif str(self.path).startswith("/echo?"):
+            # echo with a message
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
+            #get the message from the echo
             response_str = str(self.path).split("/echo?msg=",1)[1]
-            response = json.dumps({'message' : "I'm alive!!"})
             response = bytes(json.dumps({'message' : response_str}), 'utf-8')
             self.wfile.write(response)
 
         else:
+            #default 500 code to clean up loose ends
             self.send_response(500)
             self.send_header("Content-type", "text/html")
             self.end_headers()
@@ -84,8 +97,10 @@ class requestHandler(http.server.SimpleHTTPRequestHandler):
         return
 
 def run(server_class=http.server.HTTPServer, handler_class=requestHandler, addr='', port=8085):
+    # this function initializes and runs the server on the class defined above
     server_address = (addr, port)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
+#call the run function
 run()
